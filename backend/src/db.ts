117561b0365e580e8da8e2fba479d9688c27c83e
mmar58 <rahmanapu118@gmail.com>
2026-04-03 18:04:07 +0600

    Added port

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -24,6 +24,7 @@ const db = knex({
   client: "mysql2",
   connection: {
     host: process.env.DB_HOST || "127.0.0.1",
+    port: Number(process.env.DB_PORT) || 3306,
     user: process.env.DB_USER || "root",
     password: process.env.DB_PASSWORD || "123456",
     database: process.env.DB_NAME || "social_app",
